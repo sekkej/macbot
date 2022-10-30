3f532adcc3a42e460f1ebf9aa67c0a6747834d64
yer7700 <37212234+yer7700@users.noreply.github.com>
2022-10-30 13:57:44 +0300

    Fixed 'text' argument

--- cogs/neegor.py
+++ cogs/neegor.py
@@ -11,7 +11,7 @@ class NeegorLanguage(commands.Cog):
         with open('neegor_language', 'r', encoding='utf-8') as language:
             self.lang = json.loads(language.read())
 
-    async def translate(self, *, text):
+    async def translate(self, text):
         result = text.lower()
 
         for syl, trn in self.lang.items():
@@ -25,7 +25,7 @@ class NeegorLanguage(commands.Cog):
     async def neegor(self, ctx, *, text):
         translated = await self.translate(text)
 
-        embed = Embed(title = 'Neegor Language', colour = 0xf2525a, description = text)
+        embed = Embed(title = 'Neegor Language', colour = 0xf2525a, description = translated)
         embed.set_footer(text = ctx.author, icon_url = ctx.author.avatar.url)
 
         await ctx.send(embed=embed)
